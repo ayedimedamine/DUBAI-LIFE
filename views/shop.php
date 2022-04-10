780c528c2ff54c7b7a67416bc5722e4bbe84c290
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-04-11 00:47:06 +0100

    fix : remove hero.php from includes becouse the hero section for shop cress is defferent to the hero of home screen and add change for every screen to look like the design

--- views/shop.php
+++ views/shop.php
@@ -1,8 +1,27 @@
 <?php
   include 'includes/header.php';
   include 'includes/navbar.php'; 
-  include 'includes/hero.php'; 
 ?>
+<!-- Hero -->
+<div class="relative z-0 bg-white">
+  <div class="relative mx-auto lg:mx-16">
+      <img src='../assets/images/v1.0.0/hero.png' class="absolute inset-0 object-cover w-full h-full bg-black opacity-80" alt='The dubai life' />
+      <div class="bg-black">
+      <div class="flex flex-col mb-16 sm:text-center sm:mb-0">
+        <div class="relative max-w-xl mb-10 mx-auto text-center lg:max-w-2xl md:mb-12">
+          <img class='h-12 object-cover md:hidden mx-auto my-4' src='../../assets/images/v1.0.0/logo_icon.png' alt='The dubai life' />
+          <h2 class="max-w-lg mb-2 md:mt-0 text-5xl font-bold text-white sm:text-6xl mx-auto tracking-wider">
+            Jeu Concours
+          </h2>
+          <span class="border-t border-white md:pt-0 md:border-none text-white font-bold text-lg ">THE NEW LIFE BY DUBAI LIFE</span>
+          <p class="italic text-2xl md:text-3xl tracking-wide font-bold text-[#D19F46] my-4">
+            Gagnez un <span class="text-[#F3CC94]">appartement</span> sur <span class="text-[#F3CC94]">DUBAI</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
  <!-- Main -->      
 <div>
     <div class='grid grid-cols-12 gap-2 mt-8 items-start justify-center'>
