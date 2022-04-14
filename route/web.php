3ea9c4f4d1b2f648e944c88de36e33849f66c02f
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-04-14 10:18:20 +0100

    build : setup the email template

--- route/web.php
+++ route/web.php
@@ -11,7 +11,8 @@ $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/cancel', 'views/cancel.php');
     // $r->addRoute('GET', '/success', 'views//success.php?session_id={CHECKOUT_SESSION_ID}');
     $r->addRoute('GET', '/success', 'views/success.php');
-    $r->addRoute('GET', '/mail', 'views/email.html');
+    // $r->addRoute('GET', '/mail', 'views/email.html'); // EMAIL TEMPLATE VERSION 0
+    $r->addRoute('GET', '/mail', 'views/email-v1.html'); // EMAIL TEMPLATE VERSION 0
     $r->addRoute('POST', '/checkout', 'class/checking.php');
     $r->addRoute('POST', '/webhook', 'config/webhook.php');
 });
