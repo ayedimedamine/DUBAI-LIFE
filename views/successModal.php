81f576b2b3eec9202ccc37eafdbe88697358d8cb
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-04-20 15:37:59 +0100

    build : setup the success modal

--- views/successModal.php
+++ views/successModal.php
@@ -3,7 +3,7 @@
 ?>
   <div class="mt-6" x-data="{ open: true }">
     <!-- Button : Will be removed: it is used just to show how to controle modal via alpine @click eventS -->
-    <button @click="open = true" class="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline mx-auto">Open Success Modal</button>
+    <!-- <button @click="open = true" class="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline mx-auto">Open Modal(btn will be removed)</button> -->
     <!-- Modal -->
     <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open">
         <div class="h-auto mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:mx-0" @click.away="open = false">
