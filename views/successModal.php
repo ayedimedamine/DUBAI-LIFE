0abce7a944b3bb6a0c8b56573e50c5e71d5a8c23
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-04-24 03:03:12 +0100

    refactor : remove target=_blank attribute to redirect without living the same page

--- views/successModal.php
+++ views/successModal.php
@@ -7,7 +7,7 @@
     <!-- Modal -->
     <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.65);" x-show="open">
         <div class="h-auto mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:mx-0" @click.away="open = false">
-          <a href="https://the-dubai-life.com/" target="_blank">
+          <a href="https://the-dubai-life.com/">
             <img src="../assets/images/v1.0.0/pupUp.png" alt="the dubai life" class="h-[500px] w-auto object-fit"/>
          </a>  
         </div>
