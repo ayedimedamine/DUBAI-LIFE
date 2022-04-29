92beb8a6e8e21da8576368dde53dd71a60b9cec5
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-04-29 20:37:53 +0100

    latest

--- class/Database.php
+++ class/Database.php
@@ -6,7 +6,7 @@ class Database
 	static function connection()
 	{
 		try {
-			$conn = new \PDO("mysql:host=sql11.freemysqlhosting.net;dbname=sql11487395", 'sql11487395', 'kqljYbir6a');
+			$conn = new \PDO("mysql:host=sql11.freemysqlhosting.net;dbname=sql11489092", 'sql11489092', 'ipqqeJTbxz');
 	        $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		} catch (\Exception $e) {
 			 $conn = null;
