1bcd2f1966fa71c1af3cf7df65da78f51603ba9d
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-05-03 17:34:53 +0100

    fix database connection

--- class/Database.php
+++ class/Database.php
@@ -7,9 +7,11 @@ class Database
 
 	static function connection()
 	{
+		$charset = 'utf8';
 		global $DB_HOST, $DB_DATABASE, $DB_USERNAME, $DB_PASSWORD;
+		$dsn = "mysql:host={$DB_HOST};dbname={$DB_DATABASE};charset={$charset}";
 		try {
-			$conn = new \PDO("mysql:host=" . $DB_HOST . ";dbname=". $DB_DATABASE , $DB_USERNAME, $DB_PASSWORD);
+			$conn = new \PDO($dsn, $DB_USERNAME, $DB_PASSWORD);
 	        $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		} catch (\Exception $e) {
 			echo $e->getMessage();
