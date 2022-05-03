81b59744c9d460ae79b23d11b929b036bab18397
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-05-04 00:17:00 +0100

    test

--- config/config.php
+++ config/config.php
@@ -11,9 +11,15 @@ $dotenv->load();
 // $DB_PASSWORD = $_ENV['DB_PASSWORD'];
 // $DOMAIN = 'https://dubailife3.herokuapp.com';
 
-$SMTP_HOST='smtp.zoho.eu';
-$SMTP_USER='admin@the-dubai-life.com';
-$SMTP_PASSWORD='ECQsqcRcV7Qd';
+// $SMTP_HOST='smtp.zoho.eu';
+// $SMTP_USER='admin@the-dubai-life.com';
+// $SMTP_PASSWORD='ECQsqcRcV7Qd';
+// $SMTP_PORT='587';
+
+
+$SMTP_HOST='email-smtp.eu-west-3.amazonaws.com';
+$SMTP_USER='AKIAV74TIVVY2WCQADK6';
+$SMTP_PASSWORD='BDck3zc/6Fa/QY/PsmBLvisqCCYq2gfqWDZHgZT0xKfN';
 $SMTP_PORT='587';
 
 $DOMAIN = 'https://www.the-new-life.fr';
