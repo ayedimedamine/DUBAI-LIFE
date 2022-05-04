b020b845e5854143c7090f692653c918099e1f39
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-05-04 01:38:39 +0100

    latest

--- config/config.php
+++ config/config.php
@@ -1,6 +1,9 @@
 <?php
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+// TESTING
 
 $STRIPE_KEY = $_ENV['STRIPE_KEY'];
 $PRICE_ID = $_ENV['PRICE_ID'];
@@ -20,19 +23,13 @@ $SMTP_PORT = $_ENV['SMTP_PORT'];
 
 
 
-// $SMTP_HOST='smtp.zoho.eu';
-// $SMTP_USER='admin@the-dubai-life.com';
-// $SMTP_PASSWORD='ECQsqcRcV7Qd';
-// $SMTP_PORT='587';
-
-
-
 // PROD : 
 // $DOMAIN = 'https://www.the-new-life.fr';
 
 // $STRIPE_KEY = getenv('STRIPE_KEY');
 // $PRICE_ID = getenv('PRICE_ID');
 // $ENDPOINT_STRIPE_WEBHOOK_SECRET = getenv('ENDPOINT_STRIPE_WEBHOOK_SECRET');
+
 // $DB_HOST = getenv('DB_HOST');
 // $DB_DATABASE = getenv('DB_DATABASE');
 // $DB_USERNAME = getenv('DB_USERNAME');
--- config/test.php
+++ config/test.php
@@ -1,5 +1,9 @@
 <?php
-require 'config/config.php';
+// require 'config/config.php';
+$SMTP_HOST='e';
+$SMTP_USER='';
+$SMTP_PASSWORD='';
+$SMTP_PORT='587';
 require 'class/Mail.php';
 $mailer = new Mail($SMTP_USER,$SMTP_PASSWORD,$SMTP_HOST,$SMTP_PORT);
-$mailer->sendMail('ayedimedamine7@gmail.com',"dubai life","<p>hey there</p>");
+$mailer->sendMail('mohamedamine.aydi@numerys.io',"dubai life","<p>hey there</p>");
