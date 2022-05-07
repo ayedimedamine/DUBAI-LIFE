fe826c8dc41d0099a357230550de8c4b9e31c915
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-05-07 16:58:35 +0100

    share button facebook

--- includes/social.php
+++ includes/social.php
@@ -2,14 +2,14 @@
 	<div class="flex justify-center gap-x-5 mt-6 mb-8">   
 
 	 <!-- FACEBOOK  -->
-		<a class="text-white text-xl" href="https://www.facebook.com/sharer/sharer.php?u=dubailife3.herokuapp.com"  target="_blank">
+		<a class="text-white text-xl" href="https://www.facebook.com/sharer/sharer.php?u=https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif&quote=hello there&text=deccription"  target="_blank">
 		<div class="rounded-md py-2 flex px-6 items-center bg-[#1877f2] hover:shadow-md transition-all">
 			<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" class="w-5 h-5 mr-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>
 			<span> Facebook</span></div></a>
 
              <!-- Twitter  -->
 		<a class="text-white text-xl" 
-            href="https://twitter.com/intent/tweet?url=http://dubailife3.herokuapp.com/assets/images/final-2.gif"
+            href="https://twitter.com/intent/tweet?url=https://gfycat.com/difficultpoisedamericanpainthorse"
           data-action="share/twitter/share"
           target="_blank">
 		<div class="rounded-md flex items-center py-2 px-6 bg-[#1da1f2] hover:shadow-md transition-all">
@@ -19,7 +19,7 @@
 		<!-- Whatsapp -->
 		<a 
           class="text-white text-xl"
-          href="whatsapp://send?text=http://dubailife3.herokuapp.com/assets/images/final-2.gif"
+          href="whatsapp://send?gif=https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif"
           data-action="share/whatsapp/share"
           target="_blank" >
 			<div class="rounded-md flex items-center py-2 px-6 bg-[#25d366] hover:shadow-md transition-all">
