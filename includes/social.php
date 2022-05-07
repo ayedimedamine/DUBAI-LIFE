629016e863f876236f126d94fbf4c66dab482697
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-05-07 20:58:59 +0100

    refactor : adjust the sicial icons for responsive screen size

--- includes/social.php
+++ includes/social.php
@@ -1,20 +1,23 @@
-<div class="hidden md:block">
-	<div class="flex justify-center gap-x-5 mt-6 mb-8">   
+<div class=" md:block mx-6 md:mx-8">
+	<div class=" mt-6 mb-8 grid grid-cols-3 lg:grid-cols-6 gap-2">   
 
 	 <!-- FACEBOOK  -->
-		<a class="text-white text-xl" href="https://www.facebook.com/sharer/sharer.php?u=https://gfycat.com/difficultpoisedamericanpainthorse&quote=J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours ! https://www.the-new-life.fr"  target="_blank">
-		<div class="rounded-md py-2 flex px-6 items-center bg-[#1877f2] hover:shadow-md transition-all">
+	   <a class="text-white text-xl" href="https://www.facebook.com/sharer/sharer.php?u=https://gfycat.com/difficultpoisedamericanpainthorse&quote=J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours ! https://www.the-new-life.fr"  target="_blank">
+		   <div class="rounded-md py-3 flex px-2 sm:px-6 items-center justify-center bg-[#1877f2] hover:shadow-md transition-all">
 			<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" class="w-5 h-5 mr-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>
-			<span> Facebook</span></div></a>
+			<span class="hidden sm:block"> Facebook</span>
+		   </div>
+	    </a>
 
              <!-- Twitter  -->
 		<a class="text-white text-xl" 
             href="https://twitter.com/intent/tweet?text=J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours !  https://www.the-new-life.fr @thedubalife1&url=https://gfycat.com/difficultpoisedamericanpainthorse"
           data-action="share/twitter/share"
           target="_blank">
-		<div class="rounded-md flex items-center py-2 px-6 bg-[#1da1f2] hover:shadow-md transition-all">
+		<div class="rounded-md py-3 flex px-2 sm:px-6 items-center justify-center bg-[#1da1f2] hover:shadow-md transition-all">
 			<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" class="w-5 h-5 mr-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>
-			<span> Twitter</span></div></a>
+			<span class="hidden sm:block"> Twitter</span></div>
+		</a>
 
 		<!-- Whatsapp -->
 		<a 
@@ -22,33 +25,37 @@
           href="whatsapp://send?gif=https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif"
           data-action="share/whatsapp/share"
           target="_blank" >
-			<div class="rounded-md flex items-center py-2 px-6 bg-[#25d366] hover:shadow-md transition-all">
+			<div class="rounded-md py-3 flex px-2 sm:px-6 items-center justify-center bg-[#25d366] hover:shadow-md transition-all">
 				<i class="fab fa-whatsapp"></i>
-				<span class="ml-2"> Whatsapp</span>
+				<span class="hidden sm:block ml-2"> Whatsapp</span>
             </div>
         </a>
+		<!-- Telegram -->
 		<a 
           class="text-white text-xl"
+		  
           href="https://t.me/share?&text=J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours ! https://www.the-new-life.fr&url=https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif"
           data-action="share/telegram/share"
           target="_blank" >
-			<div class="rounded-md flex items-center py-2 px-6 bg-[#25d366] hover:shadow-md transition-all" style=" color: white; background-color:#229ED9; ">
+			<div class=" cursor-pointer rounded-md py-3 flex px-2 sm:px-6 items-center justify-center bg-[#0088cc] hover:shadow-md transition-all">
 				<i class="fab fa-telegram"></i>
-				<span class="ml-2"> Telegram</span>
+				<span class="hidden sm:block ml-2"> Telegram</span>
             </div>
         </a>
+		<!-- messenger TODO: add cusrsor pointer -->
 		<a class="text-white text-xl" target="_blank" >
-		<div class="rounded-md flex items-center py-2 px-6 bg-[#25d366] hover:shadow-md transition-all 	st-custom-button"style=" color: white; background-color:blue; " data-network="messenger" data-url="https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif">
-        <i class="fab fa-telegram"></i>
-				<span class="ml-2"> messenger</span>
-      </div>
+	     	<div class="rounded-md py-3 flex px-2 sm:px-6 items-center justify-center bg-[#0084ff] hover:shadow-md transition-all" data-network="messenger" data-url="https://media.giphy.com/media/BUzI1toK5LhNOidELL/giphy.gif">
+               <i class="fa-brands fa-facebook-messenger"></i>
+	      	   <span class="hidden sm:block ml-2"> messenger</span>
+            </div>
 		</a>
-		</a>
-		<a class="text-white text-xl" target="_blank" >
-		<div class="rounded-md flex items-center py-2 px-6 bg-[#25d366] hover:shadow-md transition-all 	st-custom-button"style=" color: white; background-color:blue; " data-network="sms" data-url="J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours @the-new-life,fr https://gfycat.com/difficultpoisedamericanpainthorse">
-        <i class="fab fa-telegram"></i>
-				<span class="ml-2"> sms</span>
-      </div>
+	
+        <!-- sms -->
+		<a class="text-white text-xl hover:cursor-pointer" target="_blank" >
+		    <div class="rounded-md py-2 sm:py-3 flex px-2 sm:px-6 items-center justify-center bg-[#5c73f5] hover:shadow-md transition-all" data-network="sms" data-url="J’ai pris mon ticket pour Dubaï ! Participes toi aussi au plus grand jeu concours @the-new-life,fr https://gfycat.com/difficultpoisedamericanpainthorse">
+              <svg class="w-7 h-7 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32C114.6 32 .0137 125.1 .0137 240c0 49.59 21.39 95 56.99 130.7c-12.5 50.39-54.31 95.3-54.81 95.8C0 468.8-.5938 472.2 .6875 475.2C1.1 478.2 4.813 480 8 480c66.31 0 116-31.8 140.6-51.41C181.3 440.9 217.6 448 256 448C397.4 448 512 354.9 512 240S397.4 32 256 32zM167.3 271.9C163.9 291.1 146.3 304 121.1 304c-4.031 0-8.25-.3125-12.59-1C101.1 301.8 92.81 298.8 85.5 296.1c-8.312-3-14.06-12.66-11.09-20.97S85 261.1 93.38 264.9c6.979 2.498 14.53 5.449 20.88 6.438C125.7 273.1 135 271 135.8 266.4c1.053-5.912-10.84-8.396-24.56-12.34c-12.12-3.531-44.28-12.97-38.63-46c4.062-23.38 27.31-35.91 58-31.09c5.906 .9062 12.44 2.844 18.59 4.969c8.344 2.875 12.78 12 9.906 20.34C156.3 210.7 147.2 215.1 138.8 212.2c-4.344-1.5-8.938-2.938-13.09-3.594c-11.22-1.656-20.72 .4062-21.5 4.906C103.2 219.2 113.6 221.5 124.4 224.6C141.4 229.5 173.1 238.5 167.3 271.9zM320 288c0 8.844-7.156 16-16 16S288 296.8 288 288V240l-19.19 25.59c-6.062 8.062-19.55 8.062-25.62 0L224 240V288c0 8.844-7.156 16-16 16S192 296.8 192 288V192c0-6.875 4.406-12.1 10.94-15.18c6.5-2.094 13.71 .0586 17.87 5.59L256 229.3l35.19-46.93c4.156-5.531 11.4-7.652 17.87-5.59C315.6 179 320 185.1 320 192V288zM439.3 271.9C435.9 291.1 418.3 304 393.1 304c-4.031 0-8.25-.3125-12.59-1c-8.25-1.25-16.56-4.25-23.88-6.906c-8.312-3-14.06-12.66-11.09-20.97s10.59-13.16 18.97-10.19c6.979 2.498 14.53 5.449 20.88 6.438c11.44 1.719 20.78-.375 21.56-4.938c1.053-5.912-10.84-8.396-24.56-12.34c-12.12-3.531-44.28-12.97-38.63-46c4.031-23.38 27.25-35.91 58-31.09c5.906 .9062 12.44 2.844 18.59 4.969c8.344 2.875 12.78 12 9.906 20.34c-2.875 8.344-11.94 12.81-20.34 9.906c-4.344-1.5-8.938-2.938-13.09-3.594c-11.19-1.656-20.72 .4062-21.5 4.906C375.2 219.2 385.6 221.5 396.4 224.6C413.4 229.5 445.1 238.5 439.3 271.9z"/></svg>
+			  <span class="hidden sm:block"> sms</span>
+            </div>
 		</a>
 		<!-- <a>
 		<script async src="https://telegram.org/js/telegram-widget.js?19" data-telegram-share-url="https://gfycat.com/difficultpoisedamericanpainthorse" data-text="notext"></script>
