2b73996b52607fff02aea42558b5713b419851bd
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-05-08 11:05:57 +0100

    last

--- route/web.php
+++ route/web.php
@@ -10,8 +10,8 @@ $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/cancel', 'views/home.php');
     $r->addRoute('GET', '/success', 'views/successModal.php'); // SUCCESS MODAL 
    //$r->addRoute('GET', '/mail', 'views/email-v1.html'); // EMAIL TEMPLATE VERSION 1
-   //$r->addRoute('GET', '/mail', 'views/email-v2.html'); // EMAIL TEMPLATE VERSION 2
-   $r->addRoute('GET', '/mail', 'views/email-tdl.html'); 
+   $r->addRoute('GET', '/mail', 'views/email-v2.html'); // EMAIL TEMPLATE VERSION 2
+//    $r->addRoute('GET', '/mail', 'views/email-tdl.html'); 
 
     $r->addRoute('POST', '/checkout', 'class/checking.php');
     $r->addRoute('POST', '/webhook', 'config/webhook.php');
