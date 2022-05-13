123dd8b219a754b7e1599e6956853305ad0067b9
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-05-13 17:57:36 +0100

    build : add maintenance page according to the given design

---
+++ views/maintenance.php
@@ -0,0 +1,39 @@
+<?php
+  include 'includes/header.php';
+  include 'includes/navbar.php'; 
+?>
+<!-- Main -->
+<div class="relative z-0 bg-white">
+  <div class="relative mx-auto h-screen">
+      <img src='../../assets/images/v1.0.0/maintenance.png' class="absolute inset-0 object-cover w-full h-full" alt='The dubai life' />
+      <div class="bg-black ">
+        <div class="flex flex-col mb-16 sm:text-center sm:mb-0 max-w-sm mx-auto">
+            <div class="relative max-w-xl mb-10 mx-auto text-center lg:max-w-2xl md:mb-12">
+            <div class="border border-[#D19F46] mt-16 p-4 rounded-3xl relative mx-4 md:mx-0">
+                <p class=" text-2xl md:text-3xl tracking-wide font-bold text-[#D19F46] mx-0 md:mx-8">
+                Site en maintenance
+            </p>
+            <p class=" text-lg tracking-wide font-normal text-white mt-8">
+                Notre site web sera <span class="font-bold">bientôt lancé</span>
+            </p>
+            <p class=" text-lg tracking-wide font-normal text-white">
+                <span class="font-bold">Restez</span> à l'affût des <span class="font-bold">nouvelles</span>, des <span class="font-bold">événements exclusifs</span>  et des <span class="font-bold">récompenses spéciales</span>.
+            </p>
+               <div class="flex justify-center space-x-2 text-white">
+                    <a href="https://www.instagram.com/the.dubai.life/" target="_blank">
+                        <i class="fab fa-instagram fa-2x"></i>
+                    </a>
+                    <a href="https://www.snapchat.com/add/the-dubailife" target="_blank">
+                        <i class="fab fa-snapchat fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+           
+        </div>
+       </div>
+  </div>
+</div>
+ 
+<!-- Footer -->
+<?php include('includes/footer.php'); ?>
+
