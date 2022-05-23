e928b0410b7d2a23af2dfa8314ea02404df4dc71
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-05-23 22:38:28 +0100

    latest

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -10,9 +10,7 @@ const info = document.querySelector(".alert-info");
 
 function process(event) {
   event.preventDefault();
-
   const phoneNumber = phoneInput.getNumber();
-
   info.style.display = "";
   info.innerHTML =
     "Phone number in E.164 format: <strong>${phoneNumber}</strong>";
